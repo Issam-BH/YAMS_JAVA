bdb5e4bfd291725413e133df95c8a97013243861
Yanis <yanisyaya322@gmail.com>
2025-04-20 03:33:43 +0200

    MAJ 20/04/25
    Ajout du carré et finalisation du brelan "TOK" (flm d'écrire le nom en entier)

---
+++ SoloYams/src/fr/uge/yams/Carre.java
@@ -0,0 +1,59 @@
+package fr.uge.yams;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * ATTENTION: C'est littéralement le code du brelan mais avec des modifs mineurs
+ * car meme fonctionnement il faudra changer les commentaire pour s'adapter
+ * 
+ * Représente la combinaison Carre dans le jeu de Yams.
+ * 
+ * Un carre consiste à obtenir 4 dés avec la même valeur, par exemple : [4, 4, 4, 2, 6].
+ * 
+ * Cette implémentation retourne actuellement un score fixe de 15 sans vérification,
+ * et devra être améliorée pour détecter réellement un brelan sur le plateau.
+ */
+public record Carre() implements Combination {
+
+    /**
+     * Calcule le score pour la combinaison "Three of a Kind" a.k.a Brelan. 
+     * 
+     * @param board le plateau de jeu à analyser
+     * @return valeurs de la somme des dés si brelan détecté
+     */
+	@Override
+	public int score(Board board) {
+	    Map<Integer, Integer> compteur = new HashMap<>();
+	    int totalDes = 0;
+
+	    for (Dice de : board) {
+	        int val = de.value(); 
+	        totalDes += val; // On fait le total de valeur de dé (pour éviter de faire de la multiplication)
+	        compteur.put(val, compteur.getOrDefault(val, 0) + 1);  
+	    }
+	    /*
+	     * On met la valeur au lieu du dé en lui meme
+	     * pour éviter d'avoir des dé de la meme valeur en doublon
+	     */
+
+	    for (int count : compteur.values()) { // On cherche dans les valeurs si il existe un 3
+	        if (count == 4) {
+	            return totalDes; // retourne la somme de la valeur des dés
+	        }
+	    }
+
+	    return 0; // Aucun brelan
+	}
+
+    /**
+     * Représente cette combinaison en tant que chaîne de caractères.
+     *
+     * @return "Three of A Kind"
+     */
+    @Override
+    public String toString() {
+        return "Square";
+    }
+}
